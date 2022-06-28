7fafb7ce5c3c7050df5a605bef779e2c0f5622ca
nilton404 <89037582+nilton404@users.noreply.github.com>
2022-06-28 17:39:26 -0300

    kajshaishiahs vou surtar

---
+++ public/javascripts/register.js
@@ -0,0 +1,20 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+
+const form  = document.getElementById('authy');
+
+form.addEventListener('submit', (event) => {
+    form.getAuth();
+});
